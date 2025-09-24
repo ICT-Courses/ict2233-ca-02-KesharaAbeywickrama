d19ea4c358784ad07cb493818cb864a2758ee98e
KesharaAbeywickrama <ar112936@fhss.sjp.ac.lk>
2025-09-24 22:40:13 +0530

    added 404 page

--- portofolio/src/pages/NotFound.jsx
+++ portofolio/src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4 bg-gradient-to-br from-blue-900 via-indigo-900 to-blue-800 text-white">
-      <h2 className="text-6xl md:text-7xl font-extrabold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 via-blue-400 to-indigo-400">
+      <h2 className="text-6xl md:text-7xl font-extrabold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 via-blue-400 to-indigo-400 leading-tight text-center">
         404 - Page Not Found
       </h2>
-      <p className="mb-6 text-lg md:text-xl">
+      <p className="mb-6 text-lg md:text-xl text-center">
         Oops! The page you are looking for does not exist.
       </p>
       <Link
